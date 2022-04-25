156bd69813ab7e806418fc80a0da1cc4ec8c6a1d
GHong6 <gachong@ucsc.edu>
2022-04-25 11:41:53 -0700

    lab 6

---
+++ lab6/js/lab.js
@@ -0,0 +1,35 @@
+/*
+* Author: Garrett Hong
+* Created 4/25
+*
+*/
+
+var make = "Tesla";
+var model = "Model S";
+var carYear = 2022;
+var color = "Blue";
+var ownIt = false;
+var currentYear = 2022;
+var carAge = currentYear - carYear;
+
+// console.log("Age: " + carAge);
+
+
+var myTransport = ['car', 'bike', 'walking'];
+var myMainRide = {
+  make, model, color, ownIt, currentYear, carAge
+};
+
+document.writeln("Getting around: " + myTransport + "<br>");
+
+document.writeln("My Main Ride: <pre>",
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
+
+
+
+// console.log(myMainRide);
+// console.log();
+// console.log(myTransport[1]);
+// console.log(myMainRide.make);
+//
+// console.log("My different transports: " + myTransport);
