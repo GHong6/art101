d17365940adadd040fd1ff34c42112b4ca4436c2
GHong6 <gachong@ucsc.edu>
2022-05-09 11:04:54 -0700

    lab 10

---
+++ lab10/js/lab.js
@@ -0,0 +1,27 @@
+/*
+* Author: Garrett Hong
+* Created 5/4
+*
+*/
+
+
+
+//get name
+var buttonEl = document.getElementById('button');
+buttonEl.addEventListener('click', function() {
+  var name = document.getElementById('my-input').value;
+
+  var hi = document.getElementById("p1");
+  hi.innerHTML = "Hello, " + name;
+
+  var name1 = name.split('');
+
+  //shuffle name
+  var shuffle = name1.sort(() => (Math.random() > .5) ? 1 : -1);
+  console.log("shuffle: ", shuffle.join(''));
+
+  var bye = document.getElementById("output");
+  bye.innerHTML = shuffle.join('');
+
+
+});
